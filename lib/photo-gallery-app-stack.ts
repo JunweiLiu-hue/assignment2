89ceed0fd93608420c95e8bb5e40a79e607968ab
unknown <20109222@mail.wit.ie>
2025-04-30 20:55:07 +0100

    SQS queue construction

--- lib/photo-gallery-app-stack.ts
+++ lib/photo-gallery-app-stack.ts
@@ -1,13 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';  
 
 export class PhotoGalleryAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const imagesBucket = new s3.Bucket(this, 'images');
-    
+
+    const queue = new sqs.Queue(this, 'img-created-queue', {
+      receiveMessageWaitTime: cdk.Duration.seconds(5),
+    });
+
     new cdk.CfnOutput(this, 'bucketName', {
       value: imagesBucket.bucketName,
     });
