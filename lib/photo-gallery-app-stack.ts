321533f12407b9d7fd08f60ea5572eb1991da5f7
unknown <20109222@mail.wit.ie>
2025-04-30 20:53:08 +0100

    Creat s3 bucket

--- lib/photo-gallery-app-stack.ts
+++ lib/photo-gallery-app-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class PhotoGalleryAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PhotoGalleryAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const imagesBucket = new s3.Bucket(this, 'images');
+    
+    new cdk.CfnOutput(this, 'bucketName', {
+      value: imagesBucket.bucketName,
+    });
   }
 }
